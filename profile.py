d32974b88bcc22c24680eafd7eab16214d0ba143
Bryan Parno <parno@cmu.edu>
2020-05-05 02:33:19 -0400

    2-node WAN experiment

--- profile.py
+++ profile.py
@@ -3,7 +3,7 @@
 import geni.portal as portal
 import geni.rspec.pg as pg
 
-N = 64 # CONFIGURE
+N = 2 # CONFIGURE
 BHOST = 'bulletin'
 
 # Create a portal context.
@@ -49,11 +49,13 @@ for n in range(0, M):
     j = n % N
 
     node.addService(pg.Execute(shell="sh", command="/local/repository/node.sh " + str(n) + ' ' + str(NPORT) + " >> " + output))
+    node.Site("Site" + str(n % 2))
 
     node.Site("Site" + str(n % 2))
 
 # Bulletin Execute Scripts
 bulletin.addService(pg.Execute(shell="sh", command="/local/repository/bulletin.sh"  + " >> " + output))
+bulletin.Site("Site1")
 
 bulletin.Site("Site1")
 
